32ffe33a02b4827aad93200d402e438c3b0eca28
JOSEPH <JHOPSMC@GMAIL.COM>
2025-04-11 23:53:26 -0500

    minor script changes, added release pipeline

--- init-script.sh
+++ init-script.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+set -e # Exit immediately if any command fails
+
+# Download production files from S3
 aws s3api get-object --bucket jhops881-web-server-files --key docker-compose.yaml docker-compose.yaml
 aws s3api get-object --bucket jhops881-web-server-files --key nginx.conf nginx.conf
 
+# Update domain in NGINX config from localhost to production domain
 sed -i 's/localhost/jhops\.me/g' nginx.conf # change from the dev to production domain name.
 
 # Exchange local dev builds for the production images
@@ -9,4 +13,4 @@ sed -i 's/localhost/jhops\.me/g' nginx.conf # change from the dev to production 
 sed -i 's/build: \.\/www/image: jhops881\/www-server-image:latest/g' docker-compose.yaml
 sed -i 's/build: \.\/api/image: jhops881\/api-server-image:latest/g' docker-compose.yaml
 
-docker compose up
+docker compose up -d
