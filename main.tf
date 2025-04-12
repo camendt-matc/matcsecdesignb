06cdc87a36183adc0462c482bb893b0fd4b6fffd
JOSEPH <JHOPSMC@GMAIL.COM>
2025-04-12 00:17:41 -0500

    fixed blocking of outbound traffic. likely causes init issues

--- main.tf
+++ main.tf
@@ -53,6 +53,9 @@ module "sg" {
 
   ingress_cidr_blocks = ["0.0.0.0/0"]
   ingress_rules       = ["https-443-tcp", "http-80-tcp"]
+  
+  egress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules = ["all-all"]
 
 }
 
