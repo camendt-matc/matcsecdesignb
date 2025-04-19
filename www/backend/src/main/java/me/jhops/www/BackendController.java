8634892b65b9dd7f27a14b9b9a025e2ae9f003c5
Joseph Hopwood <jhopwood@madisoncollege.edu>
2025-04-19 18:40:36 -0500

    Got a next.js server set up talking to the spring boot backend api. It displays a simple page. Next, the pages need to build out, and the deployments need to be rewritten - docker build paths and images and configuration and such to reference the new paths

---
+++ www/backend/src/main/java/me/jhops/www/BackendController.java
@@ -0,0 +1,18 @@
+package me.jhops.www;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class BackendController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from from the backend server!";
+    }
+    
+}
---
+++ www/frontend/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+        <main>
+            <h1>Hello World</h1>
+            <p>This is text</p>
+            <button>Click me!</button>
+        </main>
+    );
+}
