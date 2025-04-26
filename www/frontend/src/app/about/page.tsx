f2e42006c942a9b35b3e6d0d00b4de52dedaa0b5
Joseph Hopwood <jhopwood@madisoncollege.edu>
2025-04-26 14:59:53 -0500

    temp commit with word spaghetti

--- www/frontend/src/app/about/page.tsx
+++ www/frontend/src/app/about/page.tsx
@@ -7,8 +7,29 @@ export default function ContactPage() {
             {/* Hero Section */}
             <section className="flex flex-col items-center justify-center text-center py-20 px-6">
                 <h1 className="text-5xl md:text-6xl font-extrabold mb-4">
-                    It look like we&apos;re under construction.
+                    About Me
                 </h1>
+                <p className="text-lg text-gray-700 max-w-3xl mx-auto">
+                    My name is Joseph. I am a soon-to-graduate second year student from
+                    technical college studying IT Cloud DevOps. I'm attracted to 
+                    DevOps because of my love for the automation and optimization aspect. I'm a practical person,
+                    and I value effiency. Being able to innovate and create exciting solutions to 
+                    real problems that save time is something that feels very rewarding to me.
+                    Likewise, with cloud, I excited to work with IaC deployments. What's more
+                    is that I truly enjoy working with 
+
+                    I've always been an artist. We all are. I define art as anything made by
+                    a human - or group of them for that matter. It sometimes bothers me that
+                    modern definitions can fall short in this regard and fail to recognize
+                    beauty in this and preclude things that deserve recognition - mass fabricated
+                    tools, Even if a
+                    machine made it, someone designed the machine, it's just a longer form of
+                    artwork where you may only see one piece at a time. 
+                    Growing up, I was very much into traditional media - espeically drawing.
+                    I started drawing in kindergarten.
+                    All through my grade school years I was the class' token artist child. I
+                    enjoyed traditional media, I doodled in class, I consumed much 
+                </p>
                 <p className="text-lg text-gray-700 max-w-3xl mx-auto">
                     I appreciate your interest! Unfortunately, I don&apos;t have the about page live yet. If you&apos;d like to
                     know more, feel free to connect with me, and I&apos;d be happy to share.
